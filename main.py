adbfb7af9be1283c13f375863cea1f913b81a7ff
Ricardo Antonio Gomez Villalobos <bardack134@gmail.com>
2024-02-19 17:57:44 +0900

    verificando si el valor es numero entero o floar, o un string.

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ window.config(padx=20, pady=20)
 window.title("MILES TO KILOMETERS CONVERTER")
 
 #label guiando al usuario 
-user_info=Label(window, text="Enter the data to convert").grid(row=0, column=2, pady=2)
+user_info=Label(window, text="Enter the data to convert").grid(row=0, column=2)
 
 
 #ENTER WIDGET
@@ -22,10 +22,16 @@ user_data=StringVar()
 # Devuelve el texto ingresado por el usuario  como una cadena. 
 user_data_get=user_data.get()
 #boton de entrada de datoscls
-data_entry=Entry(window, textvariable=user_data).grid(row=1, column=2, pady=2, padx=2)
-  
+data_entry=Entry(window, textvariable=user_data, width=10).grid(row=1, column=2)
 
-
+#funcion para determinar si un dato es o no un float 
+def is_float(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+      
 
 #funcin que hace la conversion de miles a kilometros
 def calculate():
@@ -37,8 +43,11 @@ def calculate():
         #convertimos el dato a entero
         user_data_get=int(user_data_get)
     
+    elif is_float(user_data_get):
+        
+        user_data_get=float(user_data_get)
     else:
-        result=Label(window, text=" YOU DIDN'T ENTER A DIGIT ", relief=RAISED).grid(row=2, column=3, padx=2)
+        error_label=Label(window, text=" YOU DIDN'T ENTER A DIGIT ", relief=RAISED).grid(row=2, column=2)
         
     #print para probar que estamos rescatando el dato ingresado por el usario de manera correcta mostrando el dato en consola
     # print(f"user infor {user_data_get}") 
@@ -47,29 +56,33 @@ def calculate():
     result_km=user_data_get*1.6
     
     #MOSTRAR  RESULTADO EN KM
-    result=Label(window, text=result_km, relief=RAISED).grid(row=2, column=2, padx=2)
-    
+    result.config(text=f"{round(result_km, 2)}")
     
 #LABEL "MILES"
-to_miles_label=Label(window, text="to miles").grid(row=1, column=3, padx=2)
-
+to_miles_label=Label(window, text="to miles").grid(row=1, column=3)
 
 
 #LABEL "IS IQUAL TO"
-is_iqual_to=Label(window, text="is iqual to", font=('calibre',10,'bold')).grid(row=2, column=1, padx=2)
+is_iqual_to=Label(window, text="is iqual to", font=('calibre',10,'bold')).grid(row=2, column=1)
 
-
-#LABEL "KM"
-kilimeter_label=Label(window, text="Km").grid(row=2, column=3, padx=2)
+#MOSTRAR  RESULTADO EN KM
+result=Label(window, text='0')
+result.grid(row=2, column=2)
     
-
+#LABEL "KM"
+kilimeter_label=Label(window, text="Km").grid(row=2, column=3)
+    
 
 #BOTON CALCULAR
-Button(window, text='Calculate', command=calculate).grid(row=3, column=2, pady=2, padx=2)
+Button(window, text='Calculate', command=calculate).grid(row=3, column=2)
+
+
 
 
     
     
+
+
 
 
 
